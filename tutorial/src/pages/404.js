446c125a760cb660cb13301b8bce59c6f5643d56
Tatsuro <contacts@tatsuro.dev>
2020-12-09 07:25:32 +0900

    pageの作成 10

---
+++ tutorial/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+// filenameを404にしてcustom error page作成
+export default function error() {
+  return (
+    <div>
+      <h1>this is our error page</h1>
+    </div>
+  )
+}
