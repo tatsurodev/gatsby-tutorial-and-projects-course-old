fd1bc269d030ee59d915210fc0b4e6ecafb85bf6
Tatsuro <contacts@tatsuro.dev>
2020-12-09 07:36:01 +0900

    Link componentの使用 11

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello people!</div>
+  return (
+    <div>
+      Hello people!
+      <div>
+        <Link to="/blog/">blog page</Link>
+      </div>
+      <a href="https://www.gatsbyjs.org">gatsby docs</a>
+    </div>
+  )
 }
