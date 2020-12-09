4bbd4c15e883f54ab434a1b4e000226ded2b734f
Tatsuro <contacts@tatsuro.dev>
2020-12-09 10:03:49 +0900

    global cssのclass名はconflictが起こりやすい 17

--- tutorial/src/pages/blog.js
+++ tutorial/src/pages/blog.js
@@ -5,6 +5,11 @@ const blog = () => {
   return (
     <Layout>
       <h1>this is our blog page</h1>
+      <p className="blog-text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat modi,
+        labore nostrum impedit eius expedita vel mollitia, explicabo rem, nam
+        aliquid eos ad. Dicta, eos!
+      </p>
     </Layout>
   )
 }
--- tutorial/src/pages/products.js
+++ tutorial/src/pages/products.js
@@ -5,6 +5,11 @@ export default class products extends Component {
     return (
       <Layout>
         <h1>this is our products</h1>
+        <p className="products-text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+          modi, labore nostrum impedit eius expedita vel mollitia, explicabo
+          rem, nam aliquid eos ad. Dicta, eos!
+        </p>
       </Layout>
     )
   }
