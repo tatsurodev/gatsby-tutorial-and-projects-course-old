d4245b5d02df758d570f52934572a5068558a885
Tatsuro <contacts@tatsuro.dev>
2020-12-09 10:17:03 +0900

    css moduleでclass名のconflictを解消 18

--- tutorial/src/pages/blog.js
+++ tutorial/src/pages/blog.js
@@ -1,15 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
+import styles from "../components/blog.module.css"
 
 const blog = () => {
   return (
     <Layout>
-      <h1>this is our blog page</h1>
-      <p className="blog-text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat modi,
-        labore nostrum impedit eius expedita vel mollitia, explicabo rem, nam
-        aliquid eos ad. Dicta, eos!
-      </p>
+      <div className={styles.page}>
+        <h1>this is our blog page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+          modi, labore nostrum impedit eius expedita vel mollitia, explicabo
+          rem, nam aliquid eos ad. Dicta, eos!
+        </p>
+      </div>
     </Layout>
   )
 }
--- tutorial/src/pages/products.js
+++ tutorial/src/pages/products.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react"
 import Layout from "../components/layout"
+import styles from "../components/product.module.css"
+
+console.log(styles)
 export default class products extends Component {
   render() {
     return (
       <Layout>
-        <h1>this is our products</h1>
-        <p className="products-text">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
-          modi, labore nostrum impedit eius expedita vel mollitia, explicabo
-          rem, nam aliquid eos ad. Dicta, eos!
-        </p>
+        <div className={styles.page}>
+          <h1>this is our products</h1>
+          <p className={styles.text}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+            modi, labore nostrum impedit eius expedita vel mollitia, explicabo
+            rem, nam aliquid eos ad. Dicta, eos!
+          </p>
+        </div>
       </Layout>
     )
   }
