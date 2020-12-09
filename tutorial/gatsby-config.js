9b6f69752744a4beff782a382e5ae2e83a3fcaac
Tatsuro <contacts@tatsuro.dev>
2020-12-10 07:57:49 +0900

    sourceInstanceName 39

--- tutorial/gatsby-config.js
+++ tutorial/gatsby-config.js
@@ -22,5 +22,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
   ],
 }
