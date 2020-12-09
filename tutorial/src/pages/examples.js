d4bbb9066130b480f113e459b29806bdf9808de3
Tatsuro <contacts@tatsuro.dev>
2020-12-09 20:06:57 +0900

    useStaticQueryの使用 28

--- tutorial/src/pages/examples.js
+++ tutorial/src/pages/examples.js
@@ -1,10 +1,12 @@
 // rafce
 import React from "react"
+import Header from "../examples/Header"
 
 const examples = () => {
   return (
     <div>
       <h1>hello from examples page</h1>
+      <Header />
     </div>
   )
 }
