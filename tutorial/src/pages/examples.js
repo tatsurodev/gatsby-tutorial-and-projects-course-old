d680da33a591a166e6e0642a6558009a4889428c
Tatsuro <contacts@tatsuro.dev>
2020-12-10 06:49:43 +0900

    PageQueryの使用 33

--- tutorial/src/pages/examples.js
+++ tutorial/src/pages/examples.js
@@ -3,15 +3,41 @@ import React from "react"
 import Header from "../examples/Header"
 import HeaderStatic from "../examples/HeaderStatic"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = ({ data }) => {
+  const {
+    site: {
+      info: { author },
+    },
+  } = data
+
   return (
     <Layout>
       <h1>hello from examples page</h1>
       <Header />
       <HeaderStatic />
+      <h5>author: {author}</h5>
     </Layout>
   )
 }
 
+// PageQueryはcomponentからは使用不可、pages folderからのみ使用可
+// exportすることでpropsとしてgraphqlから返ってくるdataにaccessできる
+export const data = graphql`
+  query {
+    site {
+      info: siteMetadata {
+        person {
+          age
+          name
+        }
+        author
+        data
+        description
+        title
+      }
+    }
+  }
+`
 export default examples
