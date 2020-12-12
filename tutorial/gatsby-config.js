61f2258991a043fdbb51e5e78fa3d235a89c8106
Tatsuro <contacts@tatsuro.dev>
2020-12-13 06:35:03 +0900

    contentfulに接続 54 .envにapi key保存 68

--- tutorial/gatsby-config.js
+++ tutorial/gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
@@ -29,6 +32,13 @@ module.exports = {
       options: {
         name: `posts`,
         path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `el19hqpwkr40`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
