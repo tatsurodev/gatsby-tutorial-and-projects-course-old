b331a35154e1a9de3dbb7b78b5798b0875c2aceb
Tatsuro <contacts@tatsuro.dev>
2020-12-13 07:17:23 +0900

    products pageでcontentfulから取得したproductsをloop 56

--- tutorial/src/pages/products.js
+++ tutorial/src/pages/products.js
@@ -1,12 +1,40 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import styles from "../components/products.module.css"
+import Image from "gatsby-image"
+import { Link } from "gatsby"
 
-const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+const ComponentName = ({ data }) => {
+  const {
+    // {key: alias}で分割代入を別名で行う
+    allContentfulProduct: { nodes: products },
+  } = data
+
+  return (
+    <Layout>
+      <section className={styles.page}>
+        {products.map(product => {
+          return (
+            <article key={product.id}>
+              <Image fluid={product.image.fluid} alt={product.title}></Image>
+              <h3>
+                {product.title} <span>${product.price}</span>
+              </h3>
+              <Link to={`/products/${product.slug}`}>more details</Link>
+            </article>
+          )
+        })}
+      </section>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   {
     allContentfulProduct {
       nodes {
+        id
         price
         title
         slug
