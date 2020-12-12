02b0ec55a7a818216c9a73eee75b4951d12af259
Tatsuro <contacts@tatsuro.dev>
2020-12-13 08:23:38 +0900

    product template作成 59

---
+++ tutorial/src/templates/product-template.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const productTemplate = () => {
+  return (
+    <div>
+      <h1>hello from product tempalte</h1>
+    </div>
+  )
+}
+
+export default productTemplate
