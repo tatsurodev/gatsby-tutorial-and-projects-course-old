a493a9eb538b0ddcc227b8a4cf4af8c4b65fead6
Tatsuro <contacts@tatsuro.dev>
2020-12-14 01:30:54 +0900

    Hero component作成 80

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -17,7 +17,30 @@ const query = graphql`
 `
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>i'm john</h1>
+            <h4>freelance web and mobile UI/UX Designer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
