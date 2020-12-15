d8fae851a77604c61066f0b5033de5995efbc43c
Tatsuro <contacts@tatsuro.dev>
2020-12-15 09:47:39 +0900

    blogs component作成 101

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -10,6 +10,7 @@ import Blogs from "../components/Blogs"
 export default ({ data }) => {
   const {
     allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs },
   } = data
 
   return (
@@ -18,6 +19,7 @@ export default ({ data }) => {
       <Services />
       <Jobs />
       <Projects projects={projects} title="featured projects" showLink />
+      <Blogs blogs={blogs} title="latest articles" showLink />
     </Layout>
   )
 }
