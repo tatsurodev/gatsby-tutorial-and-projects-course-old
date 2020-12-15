3998c4f410f6288aa530ed6e17d90de6fe729b22
Tatsuro <contacts@tatsuro.dev>
2020-12-15 17:35:50 +0900

    sidebar作成 111

--- portfolio/src/components/Sidebar.js
+++ portfolio/src/components/Sidebar.js
@@ -2,8 +2,21 @@ import React from "react"
 import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
+
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const isOpen = true
+
+  return (
+    <aside className={`sidebar ${isOpen ? "show-sidebar" : ""}`}>
+      <button className="close-btn">
+        <FaTimes />
+      </button>
+      <div className="side-container">
+        <Links styleClass="sidebar-links" />
+        <SocialLinks styleClass="sidebar-icons" />
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
