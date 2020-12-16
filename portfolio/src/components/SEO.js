ce915310afe45e8cfcb68ed82c6b3a6061263998
Tatsuro <contacts@tatsuro.dev>
2020-12-17 03:23:06 +0900

    seo componentに動的なmeta dataを追加 126

--- portfolio/src/components/SEO.js
+++ portfolio/src/components/SEO.js
@@ -2,11 +2,36 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        siteDesc: description
+        siteUrl
+        image
+        siteTitle: title
+        twitterUsername
+      }
+    }
+  }
+`
+
 const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(query)
+  const {
+    siteDesc,
+    siteTitle,
+    siteUrl,
+    image,
+    twitterUsername,
+  } = site.siteMetadata
+
   return (
     // propsでmetaの内容を指定するか、tag内で指定する
-    <Helmet htmlAttributes={{ lang: "en" }} title={title}>
-      <meta name="description" content={description} />
+    <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
+      <meta name="description" content={description || siteDesc} />
+      <meta name="image" content={image} />
     </Helmet>
   )
 }
