a341c83b96ffcc60db32e3a00db7ba44b0274d33
Tatsuro <contacts@tatsuro.dev>
2020-12-19 08:17:38 +0900

    mdx blog project setup 133

---
+++ mdx-blog/gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  plugins: [
+    `gatsby-plugin-mdx`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-remark-images`,
+    `gatsby-plugin-preload-fonts`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-mdx`,
+    //   options: {
+    //     gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
+    //   },
+    // },
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `gatsbyjs`,
+      },
+    },
+  ],
+}
