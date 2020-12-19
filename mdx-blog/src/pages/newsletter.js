f1dd9943620d47308ab0edb9ec12a9a4cdc73892
Tatsuro <contacts@tatsuro.dev>
2020-12-20 05:38:17 +0900

    newsletter component作成 141

--- mdx-blog/src/pages/newsletter.js
+++ mdx-blog/src/pages/newsletter.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
-  return <h2>newsletter page</h2>
+  return (
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all the latest stories ot your inbox</h2>
+          <h4>I write to my frineds every few weeks</h4>
+          <form className="contact-form">
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+            />
+            <button type="submit" className="btn form-control">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export default NewsLetter
