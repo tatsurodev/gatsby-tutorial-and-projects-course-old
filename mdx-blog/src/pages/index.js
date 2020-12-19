0d23577d8799f3734c2464f92f066703dcfc61a7
Tatsuro <contacts@tatsuro.dev>
2020-12-20 04:47:46 +0900

    Hero component作成 138

--- mdx-blog/src/pages/index.js
+++ mdx-blog/src/pages/index.js
@@ -6,7 +6,11 @@ import { graphql } from 'gatsby'
 import SEO from '../components/SEO'
 // ...GatsbyImageSharpFluid
 const IndexPage = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <Hero showPerson />
+    </Layout>
+  )
 }
 
 export default IndexPage
