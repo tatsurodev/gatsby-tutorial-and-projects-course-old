acdc994ae7d80b70ec6324aeb6bd1d8e22942f34
Tatsuro <contacts@tatsuro.dev>
2020-12-21 18:34:19 +0900

    Post page作成 156

--- mdx-blog/src/pages/posts.js
+++ mdx-blog/src/pages/posts.js
@@ -3,13 +3,44 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-// ...GatsbyImageSharpFluid
+
 const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+
   return (
-    <>
+    <Layout>
       <Hero />
-    </>
+      <Posts posts={posts} title="all posts" />
+    </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "MMMM, Do, YYYY")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
 
 export default PostsPage
