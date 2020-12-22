69be5cb5e644d3a0a1caad205080c894712cdb99
Tatsuro <contacts@tatsuro.dev>
2020-12-22 11:11:45 +0900

    category templateのjsx作成 162

--- mdx-blog/src/templates/category-template.js
+++ mdx-blog/src/templates/category-template.js
@@ -4,8 +4,22 @@ import Hero from '../components/Hero'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 
-const CategoryTemplate = () => {
-  return <h2>categories template</h2>
+const CategoryTemplate = props => {
+  const {
+    pageContext: { category },
+  } = props
+  const {
+    data: {
+      categories: { nodes: posts },
+    },
+  } = props
+
+  return (
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title={`category / ${category}`} />
+    </Layout>
+  )
 }
 
 export const query = graphql`
