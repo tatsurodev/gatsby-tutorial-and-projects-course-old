05741a82d70c25f3e5a669638842c88e84473044
Tatsuro <contacts@tatsuro.dev>
2020-12-22 11:03:08 +0900

    category templateでquery作成 161

--- mdx-blog/src/templates/category-template.js
+++ mdx-blog/src/templates/category-template.js
@@ -8,4 +8,33 @@ const CategoryTemplate = () => {
   return <h2>categories template</h2>
 }
 
+export const query = graphql`
+  query GetCategories($category: String) {
+    categories: allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { frontmatter: { category: { eq: $category } } }
+    ) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "MMMM, Do, YYYY")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
+
 export default CategoryTemplate
