adf461c29c4a3eb0f1a1a1fc0a64c4d21d9c4edd
Tatsuro <contacts@tatsuro.dev>
2020-12-23 07:43:13 +0900

    categoriesをNavbarに表示 167

--- mdx-blog/src/components/Sidebar.js
+++ mdx-blog/src/components/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Links from '../constants/links'
 import Categories from '../components/Categories'
 import { IoMdClose } from 'react-icons/io'
+
 const Sidebar = ({ isOpen, toggle }) => {
   return (
     <aside className={`sidebar ${isOpen ? 'showSidebar' : ''}`}>
@@ -9,7 +10,9 @@ const Sidebar = ({ isOpen, toggle }) => {
         <IoMdClose />
       </button>
       <div className="sidebar-container">
-        <Links styleClass="sidebar-links" />
+        <Links styleClass="sidebar-links">
+          <Categories />
+        </Links>
       </div>
     </aside>
   )
