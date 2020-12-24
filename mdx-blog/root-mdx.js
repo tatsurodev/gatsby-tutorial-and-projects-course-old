2b38202d03d3524a6496430ed6fcac3f62a57bac
Tatsuro <contacts@tatsuro.dev>
2020-12-24 11:39:33 +0900

    MDX component作成 175

--- mdx-blog/root-mdx.js
+++ mdx-blog/root-mdx.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 
+const myH2 = props => {
+  if (props.title) {
+    return (
+      <h2 {...props} style={{ fontSize: '2rem', color: 'red' }}>
+        {props.children}
+      </h2>
+    )
+  }
+  return (
+    <h2 {...props} className="code">
+      {props.children}
+    </h2>
+  )
+}
+
 const components = {
-  h2: props => {
-    console.log(props)
-    return <h2 {...props}>{props.children}</h2>
-  },
+  h2: myH2,
 }
 
 export const wrapMDX = ({ element }) => {
