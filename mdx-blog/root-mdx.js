76a2d648986a205f5eb3cc2b5e1754196fa28756
Tatsuro <contacts@tatsuro.dev>
2020-12-24 16:25:24 +0900

    mdx component blockquoteを使用 179

--- mdx-blog/root-mdx.js
+++ mdx-blog/root-mdx.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings, Code } from './src/components/Complete'
+import { Headings, Code, Blockquote } from './src/components/Complete'
 
 const Testing = ({ children }) => {
-  return <code className="code">{children}</code>
+  return <div className="code">{children}</div>
 }
 
 const components = {
   h2: Headings.myH2,
   h4: Headings.myH4,
   inlineCode: Code,
+  blockquote: Blockquote,
 }
 
 export const wrapMDX = ({ element }) => {
