b75effa4a2ae3769153e392851cc2bf0d9dec86f
Tatsuro <contacts@tatsuro.dev>
2020-12-24 14:15:50 +0900

    mdx component inline codeを使用 178

--- mdx-blog/root-mdx.js
+++ mdx-blog/root-mdx.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings } from './src/components/Complete'
+import { Headings, Code } from './src/components/Complete'
+
+const Testing = ({ children }) => {
+  return <code className="code">{children}</code>
+}
 
 const components = {
   h2: Headings.myH2,
   h4: Headings.myH4,
+  inlineCode: Code,
 }
 
 export const wrapMDX = ({ element }) => {
