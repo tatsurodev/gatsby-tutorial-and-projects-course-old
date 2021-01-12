e874320cca00c8b81f976afdb1c78ea37a6f85e0
Tatsuro <contacts@tatsuro.dev>
2021-01-13 02:30:48 +0900

    Projects component作成 207

--- airtable-design-site/src/pages/index.js
+++ airtable-design-site/src/pages/index.js
@@ -12,12 +12,15 @@ import {
 import SEO from "../components/seo"
 
 const HomePage = ({ data }) => {
-  console.log(data)
+  const {
+    allAirtable: { nodes: projects },
+  } = data
 
   return (
     <Layout>
       <Hero />
       <About />
+      <Projects projects={projects} title="latest projects" />
     </Layout>
   )
 }
