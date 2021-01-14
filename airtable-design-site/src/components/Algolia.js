5626b31d28f904d208d1509ce271942e24ff8547
Tatsuro <contacts@tatsuro.dev>
2021-01-15 01:11:31 +0900

    Hitsをcustomize 229

--- airtable-design-site/src/components/Algolia.js
+++ airtable-design-site/src/components/Algolia.js
@@ -17,6 +17,18 @@ const searchClient = algoliasearch(
   process.env.GATSBY_ALGOLIA_SEARCH_KEY
 )
 
+const NewHits = connectHits(({ hits }) => {
+  return hits.map(item => {
+    const { objectID, image, name } = item
+    return (
+      <article key={objectID}>
+        <Image fluid={image} className="img" />
+        <h4>{name}</h4>
+      </article>
+    )
+  })
+})
+
 const Search = () => {
   return (
     <Wrapper>
@@ -26,7 +38,9 @@ const Search = () => {
         searchClient={searchClient}
       >
         <SearchBox />
-        <Hits />
+        <Container className="section-center">
+          <NewHits />
+        </Container>
       </InstantSearch>
     </Wrapper>
   )
